b6bce769b77625d6bc02227b3f962fbc2eec4514
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-08-04 17:52:35 +0100

    added the overefect in the appItems

--- src/components/general/AppItem.jsx
+++ src/components/general/AppItem.jsx
@@ -5,7 +5,7 @@ export const AppItem = ({icon, project})=>{
     const [isModalOpened,setIsModalOpened]=useState(false);
     return(
         <>
-        <div onClick={()=>{setIsModalOpened(!isModalOpened)}} className="bg-white rounded-full h-23 w-23 sm:w-26 sm:h-26 md:w-28 md:h-28 m-2 flex items-center 
+        <div onClick={()=>{setIsModalOpened(!isModalOpened)}} className="bg-white docItem rounded-full h-23 w-23 sm:w-26 sm:h-26 md:w-28 md:h-28 m-2 flex items-center 
         cursor-pointer 
         justify-center">
                 {icon}
