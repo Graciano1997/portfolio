a08d892b9918a486a4439afa51fc623ddaf4a11e
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-08-10 11:15:52 +0100

    update the card item color

--- src/components/App/WeatherCard.jsx
+++ src/components/App/WeatherCard.jsx
@@ -8,15 +8,15 @@ export const WeatherCard = ({data}) => {
     return (
         <div className='w-[270px] sm:w-[300px] h-[300px] bg-white rounded flex flex-col justify-between'>
             <div className="flex justify-between items-center p-2">
-                <div><h2 className="text-2xl">{ FahrenheitToCelsius(data.temp)} Cº</h2></div>
+                <div><h2 className="text-2xl text-black">{ FahrenheitToCelsius(data.temp)} Cº</h2></div>
                 <div className="flex gap-1 font-bold text-red-400"><LucideCalendar className="w-5 h-5 font-bold"/><h3>{data.datetime}</h3></div>
             </div>
 
             <div className="flex justify-center">
-                <WeatherIcon style={'w-30 h-30'} icon={data.icon} />
+                <WeatherIcon style={'w-30 h-30'} icon={data.icon}/>
             </div>
 
-            <div className="flex flex-col gap-1 items-start p-3">
+            <div className="flex flex-col gap-1 items-start p-3 text-black">
                 <p className="flex gap-1 items-center"><LucideBadgeInfo className="w-5 h-5"/> {data.conditions}</p>
                 <p className="flex gap-1 items-center"><LucideSunrise className="w-5 h-5 text-orange-600"/> Sunrise: {data.sunrise}</p>
                 <p className="flex gap-1 items-center"><LucideSunset className="w-5 h-5 text-orange-600"/> Sunset: {data.sunset}</p>
